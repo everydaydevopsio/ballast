a6753a3a07c61816aaf7efa96fffb11b6f1093c4
Mark C Allen <mark@markcallen.com>
2026-08-08 15:40:05 +0000

    Add Dart Flutter smoke coverage

---
+++ examples/smoke/dart-flutter-sample/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const Directionality(
+    textDirection: TextDirection.ltr,
+    child: Text('Ballast Dart Flutter smoke sample'),
+  ));
+}
